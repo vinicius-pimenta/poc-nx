c118015e0452ad7a32bed2e4baf3be310d27f5a1
Vinicius Pimenta <viniciustavarespimenta@gmail.com>
2023-08-14 15:57:27 -0300

    Initial commit

---
+++ apps/project-01/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'project-01',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/project-01',
+};
