6c7d90749e89e72ffd6731219461f7e73255246c
Vinicius Pimenta <viniciustavarespimenta@gmail.com>
2023-08-14 19:43:15 -0300

    adiciona arquivo para teste

--- apps/project-01/src/project-01.spec.ts
+++ apps/project-01/src/project-01.spec.ts
@@ -1,7 +1,8 @@
-import { sum } from 'utils';
+import { sum,sub } from 'utils';
 
 describe('project-01', () => {
   it('should test project-01', () => {
     expect(sum(1,1)).toEqual(2);
+    expect(sub(1,1)).toEqual(0);
   });
 });
